72062b77cc9e8c2ff2c8bed46c10274a85c1779e
Baruch Medina <54144103+ingbame@users.noreply.github.com>
2022-07-31 22:55:08 -0500

    Token preparación para todo lo demás

---
+++ MuayThaiApi/ApiCommon/Session.cs
@@ -0,0 +1,50 @@
+﻿using MuayThaiApi.Entity.Business;
+using MuayThaiApi.Entity.Security;
+using System.Security.Claims;
+
+namespace MuayThaiApi.ApiCommon
+{
+    public class Session
+    {
+        #region Patron de Diseño
+        private static Session _instance;
+        private static readonly object _instanceLock = new object();
+        public static Session Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_instanceLock)
+                    {
+                        if (_instance == null)
+                            _instance = new Session();
+                    }
+                }
+                return _instance;
+            }
+        }
+        #endregion
+        public CredentialsDtoEn GetCurrentUser(HttpContext httpContext)
+        {
+            var identity = httpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var credentialClaims = identity.Claims;
+
+                return new CredentialsDtoEn
+                {
+                    Persona = new PersonaEn
+                    {
+                        NombreCompleto = credentialClaims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value
+                    },
+                    User = new LoginDtoEn
+                    {
+                        UserName = credentialClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value
+                    }
+                };
+            }
+            return default(CredentialsDtoEn);
+        }
+    }
+}
--- MuayThaiApi/Controllers/AppController.cs
+++ MuayThaiApi/Controllers/AppController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MuayThaiApi.Core;
-using MuayThaiApi.Entity.Application;
-using MuayThaiApi.Entity.Business;
-using MuayThaiApi.Entity.Security;
-using System.Security.Claims;
 
 namespace MuayThaiApi.Controllers
 {
@@ -21,26 +17,6 @@ namespace MuayThaiApi.Controllers
             return Ok(menu.Model);
         }
         #region Private Methods
-        private CredentialsDtoEn GetCurrentUser()
-        {
-            var identity = HttpContext.User.Identity as ClaimsIdentity;
-            if (identity != null)
-            {
-                var credentialClaims = identity.Claims;
-
-                return new CredentialsDtoEn
-                {
-                    Persona = new PersonaEn
-                    {
-                        NombreCompleto = credentialClaims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value
-                    },
-                    User = new LoginDtoEn { 
-                        UserName = credentialClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value
-                    }
-                };
-            }
-            return default(CredentialsDtoEn);
-        }
         #endregion
     }
 }
